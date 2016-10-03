a87dbf5afc4295c19435c16c4b2c3da8fe21e2a1
Werner Chao <oahcrenrew@gmail.com>
2016-10-04 01:17:11 +0800

    fixed formatting and README file

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# Movie-Trailer-Website
+# Movie Trailer Website
+
+Movie Trailer Website is a website that allows users to quickly 
+view movies, general story line, and watch the trailers.
+
+## Table of Contents
+- [Quick Start] (#Quick Start)
+- [Usage] (#Usage)
+
+## Quick Start
+
+To start using this repository
+
+>  ```> $ git clone https://github.com/wernerchao/Movie-Trailer-Website.git```
+
+To run the website
+
+> ```> $ python entertainment_center.py```
+
+## What's Included
+
+Within the download you'll find the following directories and files. 
+The main files that you can edit will be:
+
+```
+Movie-Trailer-Website/
+├── entertainment_center.py
+├── fresh_tomatoes.py
+└── media.py 
+```
+
+Once you run the website, these files will be generated:
+
+```
+Movie-Trailer-Website/
+├── fresh_tomatoes.pyc
+├── fresh_tomatoes.html
+└── media.pyc 
+```
+
+## Creators
+
+**Werner Chao**
+
+- <https://github.com/wernerchao>
+
+## References
+
+We've used the following open source library to make this project:
+(https://github.com/wernerchao/ud036_StarterCode) 
+
+## Copyright and license
+
+Code and documentation copyright 2016 the Project Movie Trailer
+Authors and released under the MIT license.
+
--- entertainment_center.py
+++ entertainment_center.py
@@ -1,17 +1,44 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13Toy_Story.jpg",  # NOQA
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-avatar = media.Movie("Avatar", "A marine on an alien planet", "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg", "http://www.youtube.com/watch?v=-9ceBgWV8io")
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",  # NOQA
+                     "http://www.youtube.com/watch?v=-9ceBgWV8io")
 
-minority_report = media.Movie("Minority Report", "In 2054, where police utilize a psychic technology to arrest and convict murderers before they commit their crime", "http://www.gstatic.com/tv/thumb/movieposters/29122/p29122_p_v8_aq.jpg", "https://www.youtube.com/watch?v=wkBNuScDLtA")
+minority_report = media.Movie("Minority Report",
+                              "In 2054, where police utilize a psychic"
+                              " technology to arrest and convict murderers"
+                              " before they commit their crime",
+                              "http://www.gstatic.com/tv/thumb/movieposters/29122/p29122_p_v8_aq.jpg",  # NOQA
+                              "https://www.youtube.com/watch?v=wkBNuScDLtA")  # NOQA
 
-jumper = media.Movie("Jumper", "Aimless David Rice (Hayden Christensen) has the ability to instantly transport himself to any place he can imagine.", "http://www.gstatic.com/tv/thumb/movieposters/173063/p173063_p_v8_ab.jpg", "https://www.youtube.com/watch?v=X5ZNG3Oveh4")
+jumper = media.Movie("Jumper",
+                     "Aimless David Rice (Hayden Christensen) has"
+                     " the ability to instantly transport himself to"
+                     " any place he can imagine.",
+                     "http://www.gstatic.com/tv/thumb/movieposters/173063/p173063_p_v8_ab.jpg",  # NOQA
+                     "https://www.youtube.com/watch?v=X5ZNG3Oveh4")
 
-transformer = media.Movie("Transformers: Age of Extinction", "Autobots must escape sight from a bounty hunter who has taken control of the human", "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEwNTg1MTA5Nl5BMl5BanBnXkFtZTgwOTg2OTM4MTE@._V1_SY1000_CR0,0,640,1000_AL_.jpg", "https://www.youtube.com/watch?v=dYDGqmxMZFI")
+transformer = media.Movie("Transformers: Age of Extinction",
+                          "Autobots must escape sight from a bounty"
+                          " hunter who has taken control of the human",
+                          "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEwNTg1MTA5Nl5BMl5BanBnXkFtZTgwOTg2OTM4MTE@._V1_SY1000_CR0,0,640,1000_AL_.jpg",  # NOQA
+                          "https://www.youtube.com/watch?v=dYDGqmxMZFI")
 
-star_trek = media.Movie("Star Trek (2009)", "The brash James T. Kirk tries to live up to his father's legacy with Commander Spock keeping him in check as a vengeful, time-traveling Romulan creates black holes to destroy the Federation one planet at a time.", "https://images-na.ssl-images-amazon.com/images/M/MV5BMjE5NDQ5OTE4Ml5BMl5BanBnXkFtZTcwOTE3NDIzMw@@._V1_SY1000_CR0,0,674,1000_AL_.jpg", "https://www.youtube.com/watch?v=3PM1pvOzn_w")
+star_trek = media.Movie("Star Trek (2009)",
+                        "The brash James T. Kirk tries to live up to"
+                        " his father's legacy with Commander Spock keeping"
+                        " him in check as a vengeful, time-traveling "
+                        " Romulan creates black holes to destroy the "
+                        " Federation one planet at a time.",
+                        "https://images-na.ssl-images-amazon.com/images/M/MV5BMjE5NDQ5OTE4Ml5BMl5BanBnXkFtZTcwOTE3NDIzMw@@._V1_SY1000_CR0,0,674,1000_AL_.jpg",  # NOQA
+                        "https://www.youtube.com/watch?v=3PM1pvOzn_w")
 
 movies = [toy_story, avatar, minority_report, jumper, transformer, star_trek]
 fresh_tomatoes.open_movies_page(movies)
